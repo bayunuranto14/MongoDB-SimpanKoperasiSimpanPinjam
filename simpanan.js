a7ac108a4b87a73a3789571294c463b35dde6ae7
rahman212100 <fadhilahimam0@gmail.com>
2022-07-01 17:23:48 +0700

    Update simpanan.js

--- simpanan.js
+++ simpanan.js
@@ -22,5 +22,63 @@ const riyan = new simpanan({
     nama_nsb: "Riyan Bagus"
 });
 
+const  andi= new simpanan({
+    id_simpan: 012345,
+    tgl_simpan: 02/08/2022,
+    nama_nsb: "Andi Mulya"
+});
+
+const sarah = new simpanan({
+    id_simpan: 012345,
+    tgl_simpan: 26/11/2022,
+    nama_nsb: "Sarah Sutiawan"
+});
 
 
+const slamet = new simpanan({
+    id_simpan: 012345,
+    tgl_simpan: 22/02/2022,
+    nama_nsb: "Slamet Sejati"
+});
+
+const maryani = new simpanan({
+    id_simpan: 012345,
+    tgl_simpan: 10/04/2022,
+    nama_nsb: "Maryani Susanti"
+});
+
+const budi = new simpanan({
+    id_simpan: 012345,
+    tgl_simpan: 14/09/2022,
+    nama_nsb: "Budi Prasetyo"
+});
+
+const roy = new simpanan({
+    id_simpan: 012345,
+    tgl_simpan: 19/07/2022,
+    nama_nsb: "Roy Suryo"
+});
+
+const ragil = new simpanan({
+    id_simpan: 012345,
+    tgl_simpan: 19/07/2022,
+    nama_nsb: "Ragil Hermansyah"
+});
+
+const sri = new simpanan({
+    id_simpan: 012345,
+    tgl_simpan: 15/12/2022,
+    nama_nsb: "Sri Mulyani"
+});   
+
+simpanan.insertMany(
+    [arya, riyan, andi, sarah, slamet, maryani, budi, roy, ragil, sri],
+    function (eror) {
+        if (eror) {
+            console.log(eror);
+        } else {
+            console.log("Data Berhasil Disimpan!");
+            mongoose.Connection.close();
+        }
+    }
+);
