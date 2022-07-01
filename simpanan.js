5db0bbe2520c572b160cbafa5abbdcbb15f6d24d
rahman212100 <fadhilahimam0@gmail.com>
2022-07-01 16:49:32 +0700

    Update simpanan.js

--- simpanan.js
+++ simpanan.js
@@ -3,3 +3,24 @@ const mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost:27017/db-koperasi-simpan-pinjam");
 
 //Membuat Data
+const simpananschema = new mongoose.Schema({
+    id_simpan: Number,
+    tgl_simpan: Date,
+    nama_nsb: String,
+});
+
+const simpanan = mongoose.model("simpanan", simpananSchema);
+const arya = new simpanan({
+    id_simpan: 012345,
+    tgl_simpan: 12/05/2022,
+    nama_nsb: "Arya Susilo"
+});
+
+const riyan = new simpanan({
+    id_simpan: 012345,
+    tgl_simpan: 24/03/2022,
+    nama_nsb: "Riyan Bagus"
+});
+
+
+
