395f928abdc74cf8fc1726c1cafa39f09ab09771
Fandii21 <fandi7680@gmail.com>
2022-07-02 16:36:55 +0700

    Update transaksi.js

--- transaksi.js
+++ transaksi.js
@@ -2,47 +2,111 @@
 const mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost:27017/db-koperasi-simpan-pinjam");
 
-const pegawaiSchema = new mongoose.Schema({
-    Id_pegawai: Number,
-    Nama: String,
-    Telepon: Number,
-    Gender: String,
+const transaksiSchema = new mongoose.Schema({
+    tgl_transaksi: Number,
+    jenis_transaksi: String,
+    nama_nasabah: String,
+    gender_nsb: String,
+    telp_nsb: Number,
+    pekerjaan_nsb: String,
+    jml_transaksi: Number,
+    
    });
-   const pegawai = mongoose.model("pegawai", pegawaiSchema);
-   const fandi = new pegawai({
-    Id_Pegawai: 2019230101 ,
-    Nama: "Fandi Ahmad",
-    Telepon: 089695702303,
-    Gender: "Laki laki",
-   });
-   const bayu = new pegawai({
-    id_pegawai: 20202300903 ,
-    Nama: "Bayu Afif Nuranto",
-    Telepon: 081215430175,
-    Gender: "Laki laki",
-   });
-   const imam = new pegawai({
-    id_pegawai: 2019230103 ,
-    Nama: "Imam Fadhilah Rahman",
-    Telepon: 081917160541,
-    Gender: "Laki laki",
-   });
-   const raihan = new pegawai({
-    id_pegawai: 2019230089 ,
-    Nama: "Raihan Syah Putra Nasution",
-    Telepon: 081284343742,
-    Gender: "Laki laki",
-   });
-   const budi = new pegawai({
-    id_pegawai: 000601 ,
-    Nama: "Budiman Jaya Wijaya",
-    Telepon: 089766557923,
-    Gender: "Laki laki",
-   });
+   const transaki = mongoose.model("transaksi", transaksiSchema);
+   const arya = new transaksi({
+    tgl_transaksi: 2022-03-12,
+    jenis_transaksi: "Pinjaman" ,
+    nama_nasabah: "Arya Susilo" ,
+    gender_nsb: "Laki-Laki" ,
+    telp_nsb: 089601010202,
+    pekerjaan_nsb: "Karyawan Swasta",
+    jml_transaksi: 100000 ,
+    });
+    const rian = new transaksi({
+    tgl_transaksi: 2022-06-01,
+    jenis_transaksi: "Pinjaman" ,
+    nama_nasabah: "Rian Bagus" ,
+    gender_nsb: "Laki-Laki" ,
+    telp_nsb: 089613245678,
+    pekerjaan_nsb: "PNS",
+    jml_transaksi: 300000 ,
+    });
+    const andi = new transaksi({
+        tgl_transaksi: 2022-06-14,
+        jenis_transaksi: "Pinjaman" ,
+        nama_nasabah: "Andi Mulya" ,
+        gender_nsb: "Laki-Laki" ,
+        telp_nsb: 085512323421,
+        pekerjaan_nsb: "TNI",
+        jml_transaksi: 200000 ,
+    });
+    const sarah = new transaksi({
+        tgl_transaksi: 2022-06-28,
+        jenis_transaksi: "Pinjaman" ,
+        nama_nasabah: "Sarah Sutiawan" ,
+        gender_nsb: "Perempuan" ,
+        telp_nsb: 081288908787,
+        pekerjaan_nsb: "IRT",
+        jml_transaksi: 500000 ,
+    });
+    const slamet = new transaksi({
+        tgl_transaksi: 2022-04-23,
+        jenis_transaksi: "Pinjaman" ,
+        nama_nasabah: "Slamet Sejati" ,
+        gender_nsb: "Laki-Laki" ,
+        telp_nsb: 082117986413,
+        pekerjaan_nsb: "POLRI",
+        jml_transaksi: 700000 ,
+    });
+    const Budi = new transaksi({
+        tgl_transaksi: 2022-02-21,
+        jenis_transaksi: "Pinjaman" ,
+        nama_nasabah: "Budi Prasetyo" ,
+        gender_nsb: "Laki-Laki" ,
+        telp_nsb: 0896103204693,
+        pekerjaan_nsb: "PEDAGANG",
+        jml_transaksi: 800000 ,
+    });
+    const maryani = new transaksi({
+        tgl_transaksi: 2022-01-27,
+        jenis_transaksi: "Pinjaman" ,
+        nama_nasabah: "Maryani Susanti" ,
+        gender_nsb: "Perempuan" ,
+        telp_nsb: 0821472749530,
+        pekerjaan_nsb: "PEDAGANG",
+        jml_transaksi: 100000 ,
+    });
+    const roy = new transaksi({
+        tgl_transaksi: 2022-07-13,
+        jenis_transaksi: "Pinjaman" ,
+        nama_nasabah: "Roy Suryo" ,
+        gender_nsb: "Laki-Laki" ,
+        telp_nsb: 082117986413,
+        pekerjaan_nsb: "POLITIKUS",
+        jml_transaksi: 300000 ,
+    });
+    const ragil = new transaksi({
+        tgl_transaksi: 2022-07-19,
+        jenis_transaksi: "Pinjaman" ,
+        nama_nasabah: "Ragil Hermansyah" ,
+        gender_nsb: "Laki-Laki" ,
+        telp_nsb: 089912697805,
+        pekerjaan_nsb: "MUSISI",
+        jml_transaksi: 500000 ,
+    });
+    const sri = new transaksi({
+        tgl_transaksi: 2022-05-25,
+        jenis_transaksi: "Pinjaman" ,
+        nama_nasabah: "Sry Mulyani" ,
+        gender_nsb: "Perempuan" ,
+        telp_nsb: 082132486413,
+        pekerjaan_nsb: "",
+        jml_transaksi: 400000 ,
+    });
 
    //Mengecek apakah data sudah tersimpan atau error
     pegawai.insertMany(
-    [fandi, bayu, imam, raihan, budi],
+    [arya, rian, andi, sarah, slamet, naryabi, budi, roy, ragil, sri],
     function (error) {
     if (error) {
     console.log(error);
