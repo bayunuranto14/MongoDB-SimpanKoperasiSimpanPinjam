af0f7ace383eae4997454291041f8f17f7278dbd
Bayu Afif Nuranto <bayunuranto@gmail.com>
2022-07-02 11:15:26 +0700

    add fixed-project

--- create_data.js
+++ create_data.js
@@ -3,3 +3,12 @@ const mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost:27017/db-koperasi-simpan-pinjam");
 
 //Membuat Data
+var nasabah = require("nasabah");
+var pinjaman = require("pinjaman");
+var simpanan = require("simpanan");
+var pegawai = require("pegawai");
+
+console.log("Data Nasabah berhasil Disimpan", nasabah);
+console.log("Data pinjaman berhasil Disimpan", pinjaman);
+console.log("Data Simpanan berhasil Disimpan", simpanan);
+console.log("Data Pegawai berhasil Disimpan", pegawai);
